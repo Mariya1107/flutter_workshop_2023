3b4233708a23a60372fd53bd3fe4e365c1bb2bc8
user <mariyashaji45@gmail.com>
2023-03-29 15:42:11 +0530

    Added Random Words genaeator

---
+++ pojectflutter/android/app/src/main/kotlin/com/example/pojectflutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.pojectflutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ pojectflutter/lib/main.dart
@@ -0,0 +1,56 @@
+
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomepage()
+    );
+  }
+}
+class MyHomepage extends StatefulWidget {
+  const MyHomepage({super.key});
+
+  @override
+  State<MyHomepage> createState() => _MyHomepageState();
+}
+
+class _MyHomepageState extends State<MyHomepage> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Color.fromARGB(255, 214, 3, 3),
+          centerTitle: true,
+          title: const Text('Title'),
+        ),
+        body:Container(
+        child:Text("Welcome",style: TextStyle(fontSize: 50, color: Colors.black))
+        )
+        ),
+    );   
+  }
+}
